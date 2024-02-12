ae6dbb2474699ae3d52e3d867aeb71d9391b5b6c
Noam Rothschild <98104540+NoamRothschild@users.noreply.github.com>
2024-02-12 19:48:03 +0200

    Storing the player data in a json

--- Data/Scripts/100_Multiplayer/Player.rb
+++ Data/Scripts/100_Multiplayer/Player.rb
@@ -1,10 +1,21 @@
 Class Multiplayer
+    def self.path(file_name)
+        script_path = File.expand_path(__FILE__)
+        game_folder = File.expand_path(File.join(script_path, '..'))
+        return = File.join(game_folder, 'multiplayer', file_name)
+    end
     def self.generate_player_data
         data = {}
         data[:x] = game_player.x
         data[:y] = game_player.y
         data[:direct] = $game_player.direction
         data[:map_id] = $game_map.map_id
+        path = Multiplayer.path("Client.json")
+        File.open(path 'w') do |file|
+            file.puts data
+        ensure
+        file.close
+        end
     end
     def self.initialize
         client_info = {}
