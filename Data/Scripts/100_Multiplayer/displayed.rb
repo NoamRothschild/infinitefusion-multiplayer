65540a390e6897d812d3e73453ebbd179e5ee4d6
Noam Rothschild <98104540+NoamRothschild@users.noreply.github.com>
2024-02-13 20:15:11 +0200

    Replaced the use of "eval" with a custom function
    replaced the use of the "eval" function to evaluate a string to a hash using a new custom function to prevent malicious code execution to the server

--- Data/Scripts/100_Multiplayer/displayed.rb
+++ Data/Scripts/100_Multiplayer/displayed.rb
@@ -13,14 +13,14 @@ class DisplayedPlayer
     file_path = "client1\\Client.json" if player_num == 2
     file_path = "client2\\Client.json" if player_num == 1
     path = Multiplayer.path(file_path)
-    File.open(path, 'r') do |file| #TODO: IMPORTANT!!!!!!!! DO-NOT-USE-EVAL
+    File.open(path, 'r') do |file|
       content = file.read
       if content.empty?
         $game_variables[298] = 0
         $game_variables[299] = 0
         return #will get out of here if 2nd player is
       end
-      transferred_data = eval(content)
+      transferred_data = DisplayedPlayer.hash_eval(content)
       if transferred_data[:map_id] == $game_map.map_id #if both players are on the same map
         $game_variables[298] = transferred_data[:x]
         $game_variables[299] = transferred_data[:y]
@@ -38,5 +38,14 @@ class DisplayedPlayer
     File.open(path, 'w') do |file|
       file.write("")
     end
+  end
+
+  def self.hash_eval(string)
+    #{:x=>31, :y=>10, :direction=>6, :map_id=>77, :player_num=>1}
+    string.sub("{", "").sub("}", "").sub(":","")
+    pairs = string[1..-2].split(", ").map { |pair| pair.split("=>").map(&:strip) }
+    hash = {}
+    pairs.each { |pair| hash[pair[0][1..-1].to_sym] = pair[1].to_i }
+    return hash
   end
 end
